2c195dfce7ed652b8a7221ccfcc4942c99971349
Hanz <hanz@godot.id>
2022-11-15 19:10:52 +0700

    feat(interpreter/enums): Added ARG_TYPE

--- interpreter/enums.js
+++ interpreter/enums.js
@@ -1,7 +1,7 @@
 const COMMANDS_NAME = [
 	/* Reserved names */
 	'EOF',
-	'_string', '_int',
+	'_reserved', '_reserved0',
 	'_reserved1', '_reserved2',
 	'_reserved3', '_reserved4',
 	'_reserved5', '_reserved6',
@@ -9,7 +9,7 @@ const COMMANDS_NAME = [
 	'_reserved9', '_reserved10',
 
 	/* Functions */
-	'add'
+	'mov', 'add'
 ];
 const COMMANDS = {};
 
@@ -17,7 +17,29 @@ for (let i = 0; i < COMMANDS_NAME.length; i++) {
 	COMMANDS[COMMANDS_NAME[i].toUpperCase()] = i;
 }
 
+const ARG_TYPE_NAME = [
+	/* Reserved */
+	'void',
+	'_reserved', '_reserved0',
+	'_reserved1', '_reserved2',
+	'_reserved3', '_reserved4',
+	'_reserved5', '_reserved6',
+	'_reserved7', '_reserved8',
+	'_reserved9', '_reserved10',
+
+	/* Primitives */
+	'string', 'int'
+];
+const ARG_TYPE = {};
+
+for (let i = 0; i < ARG_TYPE_NAME.length; i++) {
+	ARG_TYPE[ARG_TYPE_NAME[i].toUpperCase()] = i;
+}
+
 module.exports = {
 	COMMANDS,
-	COMMANDS_NAME
+	COMMANDS_NAME,
+
+	ARG_TYPE_NAME,
+	ARG_TYPE
 };
