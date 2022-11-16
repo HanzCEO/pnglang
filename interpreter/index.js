da36b6d58f9df4295ddfe51d91a57ddd9659fba8
Hanz <hanz@godot.id>
2022-11-17 06:01:01 +0700

    feat(interpreter/index): Added call store procedure

--- interpreter/index.js
+++ interpreter/index.js
@@ -1,7 +1,37 @@
 const Jimp = require('jimp');
 
-const { COMMANDS_NAME } = require('./enums');
+const { COMMANDS_NAME, ARG_TYPE } = require('./enums');
 const Environment = require('./environment');
+
+function processArgument(arg) {
+	if (arg === '\x00') {
+		// void
+		return { type: ARG_TYPE.VOID };
+	} else if (arg.startsWith('"')) {
+		// String
+		let val = '';
+		let escaped = false;
+
+		for (const ch of arg) {
+			if (ch == '\\') {
+				escaped = true;
+				continue;
+			} else if (ch == '"' && !escaped) {
+				break;
+			}
+
+			val += ch;
+		}
+
+		// Then remove the prefix '"'
+		val = val.substr(1);
+
+		return {
+			type: ARG_TYPE.STRING
+			value: val
+		};
+	}
+}
 
 function parseArguments(_args) {
 	let args = _args.split(' ');
@@ -13,6 +43,8 @@ function parseArguments(_args) {
 	if (args.length < 2) {
 		args = args.concat('\x00'.repeat(2 - args.length).split(''));
 	}
+
+	args = args.map(processArgument);
 
 	return args;
 }
@@ -33,6 +65,7 @@ function parse(asm) {
 		let args = line.substr(command.length + 1);
 
 		let [arg1, arg2] = parseArguments(args);
+
 		retval.push([command, arg1, arg2]);
 	}
 
@@ -43,8 +76,78 @@ function asmToCode(asm) {
 	let codeset = parse(asm);
 	let code = [];
 
+	/* @v1
+	 * 0) [CM&, \x00, \x00]
+	 * 1) [COMMAND, ARG1_TYPE, ARG2_TYPE]
+	 * 2) [IS_LENGTH_END_DEFINITION, ARG1_LENGTH, ARG2_LENGTH]
+	 * ... // until IS_LENGTH_END_DEFINITION byte is not 0
+	 * 3) [__RESERVED__, ARG1_PART, ARG2_PART]
+	 * ... // until it finishes
+	 * 4) [CM&, \x01, \x01]
+	 */
+
 	for (const [command, arg1, arg2] of codeset) {
-		code.push([COMMANDS_NAME.indexOf(command), arg1.charCodeAt(0), arg2.charCodeAt(0)]);
+		let args = [arg1, arg2];
+		let valCodes = [[], []];
+
+		for (let i = 0; i < args.length; i++) {
+			let arg = args[i];
+
+			switch (arg.type) {
+			case ARG_TYPE.VOID:
+				valCodes[i] = [0];
+				break;
+			case ARG_TYPE.STRING:
+				valCodes[i] = arg.value.split('').map(ch => ch.charCodeAt(0));
+				break;
+			case ARG_TYPE.INT:
+				let hex = Number(arg.value).toString(16);
+				for (let p = 0; p < hex.length; p += 2) {
+					valCodes[i].push(
+						parseInt(hex.substr(p, 2), 16)
+					);
+				}
+				break;
+			}
+		}
+
+		// [CM&, \x00, \x00]
+		code.push(COMMANDS['cm&'], 0, 0]);
+
+		// 1) [COMMAND, ARG1_TYPE, ARG2_TYPE]
+		code.push([COMMANDS_NAME.indexOf(command), arg1.type, arg2.type]);
+
+		/*
+		 * 2) [IS_LENGTH_END_DEFINITION, ARG1_LENGTH, ARG2_LENGTH]
+		 * ... // until IS_LENGTH_END_DEFINITION byte is not 0
+		 */
+		let lengths = [arg1.value.length, arg2.value.length];
+		let lengthMax = Math.max(arg1.value.length, arg2.value.length);
+		// Remember: 255 is 255 not 256 as in 0-index
+		for (let i = 0; i < Math.floor(lengthMax / 255); i++) {
+			lengths[0] -= 255;
+			lengths[1] -= 255;
+
+			let l1 = lengths[0] < 0 ? 0 : 255;
+			let l2 = lengths[1] < 0 ? 0 : 255;
+			code.push([0, l1, l2]);
+		}
+		code.push([1, Math.max(0, lengths[0]), Math.max(0, lengths[1])]);
+
+		/*
+		 * 3) [__RESERVED__, ARG1_PART, ARG2_PART]
+		 * ... // until it finishes
+		 */
+		let arg1val = arg1.value.toString().split('').map(ch => ch.charCodeAt(0));
+		let arg2val = arg2.value.toString().split('').map(ch => ch.charCodeAt(0));
+
+		let argmaxval = Math.max(arg1val.length, arg1val.length);
+		for (let i = 0; i < argmaxval; i++) {
+			code.push([0, arg1val[i] ?? 0, arg2val[i] ?? 0]);
+		}
+
+		// [CM&, \x01, \x01]
+		code.push(COMMANDS['cm&'], 1, 1]);
 	}
 
 	return code;
