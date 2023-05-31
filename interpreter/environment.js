7a24648c9d1f90123ed73b3c28e5a2acf417fb7d
Hanz <hanz@godot.id>
2023-05-31 16:47:09 +0700

    feat(interpreter/evironment): Added proper function call handler

--- interpreter/environment.js
+++ interpreter/environment.js
@@ -1,4 +1,44 @@
-const { COMMANDS } = require('./enums');
+const { COMMANDS, ARG_TYPE } = require('./enums');
+
+function decodeArgValue(type, val) {
+	switch (type) {
+	case ARG_TYPE['STRING']:
+		return String.fromCharCode(val);
+	case ARG_TYPE['INT']:
+		return String.fromCharCode(val);
+		// return String(val)[0];
+	}
+}
+
+function parseFunctionCall(code, i) {
+	let detail = {};
+
+	// Parse function id and args types
+	detail.command = code[i][0];
+	detail.argsType = [code[i][1], code[i][2]];
+	i++;
+
+	// Parse args length
+	detail.argsLength = [0, 0];
+	while (code[i][0] != COMMANDS['ARGPART']) {
+		detail.argsLength[0] += code[i][1];
+		detail.argsLength[1] += code[i][2];
+		i++;
+	}
+
+	// Parse args content
+	detail.argsValue = ['', ''];
+	while (code[i][0] == COMMANDS['ARGPART']) {
+		detail.argsValue[0] += decodeArgValue(detail.argsType[0], code[i][1]);
+		detail.argsValue[1] += decodeArgValue(detail.argsType[1], code[i][2]);
+		i++;
+	}
+
+	return {
+		i,
+		detail
+	};
+}
 
 module.exports = class Environment {
 	constructor() {
@@ -11,15 +51,22 @@ module.exports = class Environment {
 	}
 
 	run(code) {
-		for (const instruction of code) {
-			switch (instruction[0]) {
-			case COMMANDS.ADD:
-				console.log(
-					(instruction[1] - '0'.charCodeAt(0))
-					+
-					(instruction[2] - '0'.charCodeAt(0))
-				);
-				break;
+		for (let i = 0; i < code.length; i++) {
+			const instruction = code[i];
+			if (instruction[0] > 12) {
+				let evaluation = parseFunctionCall(code, i);
+				i = evaluation.i;
+				let detail = evaluation.detail;
+
+				switch (detail.command) {
+				case COMMANDS.ADD:
+					console.log(
+						(detail.argsValue[0] - '0')
+						+
+						(detail.argsValue[1] - '0')
+					);
+					break;
+				}
 			}
 		}
 	}
